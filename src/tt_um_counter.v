707a85f60ddbef8aece19c811460b8b6426c2969
Zahra-jahanpeyma <77167575+Zahra-jahanpeyma@users.noreply.github.com>
2025-06-07 10:05:27 +0330

    Create tt_um_counter.v

---
+++ src/tt_um_counter.v
@@ -0,0 +1,22 @@
+module tt_um_counter (
+    input clk,              
+    input ena,              
+    input [7:0] ui_in,      
+    output [7:0] uo_out     
+);
+
+    reg [3:0] count;
+
+    always @(posedge clk) begin
+        if (ena) begin
+            if (ui_in[0]) begin
+                count <= 4'b0000;
+            end else begin
+                count <= count + 1;
+            end
+        end
+    end
+
+    assign uo_out = {4'b0000, count};  
+
+endmodule
